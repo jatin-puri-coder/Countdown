ad3083cc90e1716d3d896cd2bb86d5a221563387
Jatin-Puri <jatinpuri11@gmail.com>
2021-07-01 13:17:57 +1200

    add CSS and positioning elements

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ scene.add(sphere)
 
 //Base Camera
 camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.set(0, -2, 2)
+camera.position.set(0, -1, 3)
 scene.add(camera)
 
 //Renderer
